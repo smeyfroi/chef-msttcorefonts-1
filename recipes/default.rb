a97758b8869ea83ac0d3d95dcd706f9bcc242abe
Russell Osborne <russosborn@gmail.com>
2014-05-05 23:11:19 -0400

    Removing Custom font config as it totally borks kerning for Times new roman.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,14 +12,6 @@ case node["platform"]
   when "ubuntu"
     include_recipe "chef-msttcorefonts::debian"
 end
-
-#---------------
-# install improved local.conf
-#---------------
-# from https://wiki.ubuntu.com/Fonts
-cookbook_file "/etc/fonts/local.conf" do
-  source "localfonts.conf"
-  mode 0644
 end
 
 #---------------
