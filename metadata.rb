2f0cce86f06188358be47771bf303e67fa13d0ec
Russell Osborne <russosborn@gmail.com>
2014-10-17 11:16:21 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'frank.hoffsummer@gmail.com'
 license 'Apache 2.0'
 description 'Installs Microsoft corefonts on linux boxes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.11'
+version '0.12'
 
 depends 'yum'
 depends 'apt'
